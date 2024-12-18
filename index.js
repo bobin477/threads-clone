5ae8e8a086325148e7fa935a85759f5109c17464
Hoàng Chương <hoangchuong@MacBook-Pro-cua-Hoang.local>
2024-12-19 02:17:10 +0700

    active

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ document.addEventListener("click", (e) => {
     popupMenu.style.display = "none";
   }
 });
+
+const arrIcon = ["fa-house", "fa-user", "fa-plus", "fa-cogs", "fa-phone"];
+const menu = document.querySelector(".menu");
+
+arrIcon.map((item, index) => {
+  let addClassAdd = "";
+  if (item === "fa-plus") {
+    addClassAdd = "add";
+  }
+
+  return (menu.innerHTML += `
+     <div class="menu-item ${addClassAdd}">
+      <a href="#" class="${index == 0 ? "active" : ""}" >
+        <i class="fa-solid ${item} "></i>
+      </a>
+    </div>
+  `);
+});
+
+const menuItems = document.querySelectorAll(".menu-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    document
+      .querySelectorAll(".menu-item a")
+      .forEach((el) => el.classList.remove("active"));
+    item.querySelector(".menu-item a").classList.add("active");
+  });
+});
