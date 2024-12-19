a589d57fab995e86650154bd00a373183d9cbac6
Hoàng Chương <hoangchuong@MacBook-Pro-cua-Hoang.local>
2024-12-19 10:32:21 +0700

    refactor

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 const profileIcon = document.getElementById("profile-icon");
 const popupMenu = document.getElementById("popup-menu");
 
-profileIcon.addEventListener("click", (e) => {
+function openPopupmenu(e) {
   popupMenu.style.display =
     popupMenu.style.display === "block" ? "none" : "block";
   console.log(popupMenu.style.display);
-});
+}
 
-document.addEventListener("click", (e) => {
-  console.log(profileIcon.contains(e.target));
+function handleClickOut(e) {
   if (!profileIcon.contains(e.target)) {
     popupMenu.style.display = "none";
   }
-});
+}
 
-const arrIcon = ["fa-house", "fa-user", "fa-plus", "fa-cogs", "fa-phone"];
-const menu = document.querySelector(".menu");
+document.addEventListener("click", handleClickOut);
 
-arrIcon.map((item, index) => {
-  let addClassAdd = "";
-  if (item === "fa-plus") {
-    addClassAdd = "add";
-  }
+function handleOnclickActive(el) {
+  document
+    .querySelectorAll(".menu-item a")
+    .forEach((el) => el.classList.remove("active"));
+  el.querySelector(".menu-item a").classList.add("active");
+}
 
-  return (menu.innerHTML += `
-     <div class="menu-item ${addClassAdd}">
-      <a href="#" class="${index == 0 ? "active" : ""}" >
-        <i class="fa-solid ${item} "></i>
-      </a>
-    </div>
-  `);
-});
+function renderAppbarMenu() {
+  const arrIcon = ["fa-house", "fa-user", "fa-plus", "fa-cogs", "fa-phone"];
+  const menu = document.getElementById("menu");
 
-const menuItems = document.querySelectorAll(".menu-item").forEach((item) => {
-  item.addEventListener("click", () => {
-    document
-      .querySelectorAll(".menu-item a")
-      .forEach((el) => el.classList.remove("active"));
-    item.querySelector(".menu-item a").classList.add("active");
+  arrIcon.map((item, index) => {
+    let addClassAdd = "";
+    if (item === "fa-plus") {
+      addClassAdd = "add";
+    }
+
+    return (menu.innerHTML += `
+       <div class="menu-item ${addClassAdd}" onclick="handleOnclickActive(this)">
+        <a href="#" class="${index == 0 ? "active" : ""}" >
+          <i class="fa-solid ${item} "></i>
+        </a>
+      </div>
+    `);
   });
-});
+}
+
+renderAppbarMenu();
